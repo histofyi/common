2fc44c9e7c45a92f1e5ba2f6abc14fbb4c59eef9
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-05-28 14:06:03 +0100

    Updated the filter for title to have a fallback if the structure is not yet assigned

--- filters.py
+++ filters.py
@@ -1,7 +1,6 @@
 
 
 def class_i_display_name(allele):
-    print(allele)
     if allele is not None:
         if 'h2-' in allele:
             display_name = f'{allele[0].upper()}{allele[1:3]}{allele[3].upper()}{allele[4:]}'
@@ -18,9 +17,12 @@ def class_i_display_name(allele):
 
 def structure_title(structure):
     title = ''
-    if structure['complex']['slug'] == 'class_i_with_peptide':
-        allele = class_i_display_name(structure["allele"]["mhc_alpha"])
-        title = f'{allele} binding {structure["peptide"]["sequence"]} at {structure["resolution"]}&#8491; resolution'
+    if 'slug' in structure['complex']:
+        if structure['complex']['slug'] == 'class_i_with_peptide':
+            allele = class_i_display_name(structure["allele"]["mhc_alpha"])
+            title = f'{allele} binding {structure["peptide"]["sequence"]} at {structure["resolution"]}&#8491; resolution'
+    else:
+        title = f'Currently unassigned - "{structure["publication"]["citation"]["title"]}"'
     return title
 
 
