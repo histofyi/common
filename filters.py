bd724a3c9a88365d6494c8005dd073f0b3ae8718
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-05-28 14:15:20 +0100

    Improvements to the title filer

--- filters.py
+++ filters.py
@@ -18,9 +18,20 @@ def class_i_display_name(allele):
 def structure_title(structure):
     title = ''
     if 'slug' in structure['complex']:
-        if structure['complex']['slug'] == 'class_i_with_peptide':
+        if structure["allele"] is None:
+            if structure['organism'] is not None:
+                allele = structure['organism']['common_name'] + ' MHC Class I'
+        else:
             allele = class_i_display_name(structure["allele"]["mhc_alpha"])
+        if structure['complex']['slug'] == 'class_i_with_peptide':
             title = f'{allele} binding {structure["peptide"]["sequence"]} at {structure["resolution"]}&#8491; resolution'
+        if structure['complex']['slug'] in ['class_i_with_peptide_and_alpha_beta_tcr', 'class_i_with_peptide_and_alpha_beta_tcr']:
+            if 'alpha_beta' in structure['complex']['slug']:
+                tcr_type = 'Alpha/Beta'
+            else:
+                tcr_type = 'Delta/Gamma'
+            title = f'{allele} presenting {structure["peptide"]["sequence"]} to {tcr_type} T cell receptor at {structure["resolution"]}&#8491; resolution'
+
     else:
         title = f'Currently unassigned - "{structure["publication"]["citation"]["title"]}"'
     return title
