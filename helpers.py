a3ad64cc94fe5d33bf09a3c928c03597d019fd0a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-01 20:57:13 +0100

    Added an extra character to slugify

--- helpers.py
+++ helpers.py
@@ -30,7 +30,7 @@ class NonHetSelect(Select):
 
 def slugify(string):
     slug_char = '_'
-    to_replace = [' ','-','.',',','[',']','{','}','(',')','/','\\','*']
+    to_replace = [' ','-','.',',','[',']','{','}','(',')','/','\\','*',':']
     for replace_me in to_replace:
         if replace_me in string:
             string = string.replace(replace_me, slug_char)
