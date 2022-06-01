af8e8eec6db8c672f614620e3edaad4528e0b433
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-01 20:57:35 +0100

    Added many more clauses to the structure title filter

--- filters.py
+++ filters.py
@@ -17,51 +17,54 @@ def class_i_display_name(allele):
 
 def structure_title(structure, short=False):
     title = None
-    if 'complex' in structure:
-        print ('has complex')
-        if 'slug' in structure['complex']:
-            print ('has slug')
-            print (structure['complex']['slug'])
-            if structure["allele"] is None:
-                if structure['organism'] is not None:
-                    allele = structure['organism']['common_name'].capitalize() + ' MHC Class I'
-            else:
-                allele = class_i_display_name(structure["allele"]["mhc_alpha"])
-            if structure['complex']['slug'] == 'class_i_with_peptide':
-                title = f'{allele} binding {structure["peptide"]["sequence"]} at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_tapbpr':
-                title = f'{allele} binding TAPBR at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_possibly_without_peptide':
-                title = f'{allele} with no distinct peptide at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_ly49c':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and Ly49c NK receptor at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_kir':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and KIR NK receptor at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_lir2':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and LIR-2 at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_lir1':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and LIR-1 at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_us2':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and human cytomegalovirus protein US2 at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] == 'class_i_with_peptide_and_e3':
-                title = f'{allele} with {structure["peptide"]["sequence"]} and human adenovirus protein E3 at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] in ['class_i_with_peptide_and_alpha_beta_tcr', 'class_i_with_peptide_and_gamma_delta_tcr']:
-                print ('is tcr')
-                if 'alpha_beta' in structure['complex']['slug']:
-                    tcr_type = 'Alpha/Beta'
+    if structure:
+        if 'complex' in structure:
+            print ('has complex')
+            if 'slug' in structure['complex']:
+                print ('has slug')
+                print (structure['complex']['slug'])
+                if structure["allele"] is None:
+                    if structure['organism'] is not None:
+                        allele = structure['organism']['common_name'].capitalize() + ' MHC Class I'
                 else:
-                    tcr_type = 'Gamma/Delta'
-                title = f'{allele} presenting {structure["peptide"]["sequence"]} to {tcr_type} T-cell receptor at {structure["resolution"]}&#8491; resolution'
-            if structure['complex']['slug'] in ['class_i_with_peptide_and_cd8', 'class_i_with_peptide_and_cd8_dimer']:
-                title = f'{allele} binding {structure["peptide"]["sequence"]} with CD8 at {structure["resolution"]}&#8491; resolution'
-    if not title:
-        if 'publication' in structure:
-            title = f'"{structure["publication"]["citation"]["title"]}"'
+                    allele = class_i_display_name(structure["allele"]["mhc_alpha"])
+                if structure['complex']['slug'] == 'class_i_with_peptide':
+                    title = f'{allele} binding {structure["peptide"]["sequence"]} at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_tapbpr':
+                    title = f'{allele} binding TAPBR at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_possibly_without_peptide':
+                    title = f'{allele} with no distinct peptide at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_ly49c':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and Ly49c NK receptor at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_kir':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and KIR NK receptor at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_lir2':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and LIR-2 at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_lir1':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and LIR-1 at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_us2':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and human cytomegalovirus protein US2 at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] == 'class_i_with_peptide_and_e3':
+                    title = f'{allele} with {structure["peptide"]["sequence"]} and human adenovirus protein E3 at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] in ['class_i_with_peptide_and_alpha_beta_tcr', 'class_i_with_peptide_and_gamma_delta_tcr']:
+                    print ('is tcr')
+                    if 'alpha_beta' in structure['complex']['slug']:
+                        tcr_type = 'Alpha/Beta'
+                    else:
+                        tcr_type = 'Gamma/Delta'
+                    title = f'{allele} presenting {structure["peptide"]["sequence"]} to {tcr_type} T-cell receptor at {structure["resolution"]}&#8491; resolution'
+                if structure['complex']['slug'] in ['class_i_with_peptide_and_cd8', 'class_i_with_peptide_and_cd8_dimer']:
+                    title = f'{allele} binding {structure["peptide"]["sequence"]} with CD8 at {structure["resolution"]}&#8491; resolution'
+        if not title:
+            if 'publication' in structure:
+                title = f'"{structure["publication"]["citation"]["title"]}"'
+            else:
+                title = 'Currently unassigned'
         else:
-            title = 'Currently unassigned'
+            if not short:
+                title = f'{title}<br /><small>Title : "{structure["publication"]["citation"]["title"]}"</small>'
     else:
-        if not short:
-            title = f'{title}<br /><small>Title : "{structure["publication"]["citation"]["title"]}"</small>'
+        title = 'Unknown'
     return title
 
 
