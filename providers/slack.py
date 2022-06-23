adf3be7ae64038a96316bf774d38d113b5802fdb
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-23 22:11:08 +0100

    Removed print statements from debugging

--- providers/slack.py
+++ providers/slack.py
@@ -11,14 +11,11 @@ class slackProvider():
     webhook = None
 
     def __init__(self, webhook:str):
-        print (webhook)
         self.webhook = webhook
 
 
     def send(self, template:str, variables:Dict):
         message = render_template(f'shared/slack/{template}.jnj', **variables)
-        print (message)
         response = httpProvider().post(self.webhook, message, 'txt')
-        print (response)
         return response
 
