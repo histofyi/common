e3d6262d4f1083487112c15b5d24de844bf70f4a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-23 20:38:51 +0100

    Added 404 method to Plausible

--- providers/plausible.py
+++ providers/plausible.py
@@ -12,13 +12,24 @@ class plausibleProvider():
         self.domain = domain
 
 
-    def empty_search(self, query:str) -> Tuple[Dict, bool, List]:
-        errors = []
-        search_url = f'https://www.histo.fyi/search?query={query}'
-        payload = {'name': 'EmptySearch', 'domain': 'histo.fyi','url':search_url,'props':{'query':query}}
+    def handle_event(self, event_name:str, site_url:str, props:Dict) -> Tuple[Dict, bool, List]:
         headers = {'Content-type': 'application/json'}
+        payload = {'name': event_name, 'domain': 'histo.fyi','url':site_url,'props':props}
         r = requests.post(self.url, data=json.dumps(payload), headers=headers)
         if r.status_code == 200:
-            return {'return':r.content}, True, errors
+            return {'return':r.content}, True, []
         else:
             return {'return':r.content}, False, ['plausible_errors']
+
+
+
+    def empty_search(self, query:str) -> Tuple[Dict, bool, List]:
+        search_url = f'https://www.histo.fyi/search?query={query}'
+        return self.handle_event('EmptySearch', search_url, {'query':query})
+
+
+    def record_404(self, path:str) -> Tuple[Dict, bool, List]:
+        error_url = f'https://www.histo.fyi/{path}'
+        return self.handle_event('404', error_url, {'path':f'/{path}'})
+
+
