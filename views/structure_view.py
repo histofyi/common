ae8cf6eafb87d5d0a5fa4aba206fe94dc5eb8f0c
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-29 17:57:31 +0100

    Added a function to hide non-classical for now
    #TODO remove this when non-classical display is meaningful

--- views/structure_view.py
+++ views/structure_view.py
@@ -7,7 +7,15 @@ from common.helpers import fetch_constants, fetch_core, fetch_facet
 
 def structure_view(pdb_code):
     blocks = ['chains', 'allele_match', 'aligned', 'peptide_matches', 'peptide_neighbours', 'peptide_structures', 'pockets', 'peptide_angles', 'cleft_angles', 'c_alpha_distances']
+    message = 'This structure is of a non-classical Class I molecule or a homologue. These structures will be available in a future release.'
+    display = False
     structure, success, errors = fetch_core(pdb_code, current_app.config['AWS_CONFIG'])
+    if 'complex' in structure:
+        if 'components' in structure['complex']:
+            if 'class_i_alpha' in structure['complex']['components']:
+                display = True
+                message = None
+    print (structure['complex'])
     associated_structures = {}
     if success:
         structure['pdb_code'] = pdb_code
@@ -20,4 +28,4 @@ def structure_view(pdb_code):
                 thisstructure, success, errors = fetch_core(associated_structure, current_app.config['AWS_CONFIG'])
                 if success and associated_structure != pdb_code:
                     associated_structures[associated_structure] = thisstructure
-    return {'structure':structure, 'pdb_code':pdb_code, 'features':fetch_constants('features'), 'chain_types':fetch_constants('chains'), 'associated_structures':associated_structures}
+    return {'structure':structure, 'pdb_code':pdb_code, 'features':fetch_constants('features'), 'chain_types':fetch_constants('chains'), 'associated_structures':associated_structures, 'display': display, 'message':message}
