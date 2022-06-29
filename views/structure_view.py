7325d4fbc5760f629e051dbd857ac2c90179755a
Chris Thorpe <33100149+drchristhorpe@users.noreply.github.com>
2022-06-29 21:12:22 +0100

    Added in functionality to reduce requests to AWS if the article is set to not be displayed

--- views/structure_view.py
+++ views/structure_view.py
@@ -15,17 +15,17 @@ def structure_view(pdb_code):
             if 'class_i_alpha' in structure['complex']['components']:
                 display = True
                 message = None
-    print (structure['complex'])
     associated_structures = {}
-    if success:
-        structure['pdb_code'] = pdb_code
-        structure['facets'] = {}
-        for block in blocks:
-            block_data, success, errors = fetch_facet(pdb_code, block, current_app.config['AWS_CONFIG'])
-            structure['facets'][block] = block_data
-        if structure['associated_structures'] is not None:
-            for associated_structure in structure['associated_structures']:
-                thisstructure, success, errors = fetch_core(associated_structure, current_app.config['AWS_CONFIG'])
-                if success and associated_structure != pdb_code:
-                    associated_structures[associated_structure] = thisstructure
+    if display:
+        if success:
+            structure['pdb_code'] = pdb_code
+            structure['facets'] = {}
+            for block in blocks:
+                block_data, success, errors = fetch_facet(pdb_code, block, current_app.config['AWS_CONFIG'])
+                structure['facets'][block] = block_data
+            if structure['associated_structures'] is not None:
+                for associated_structure in structure['associated_structures']:
+                    thisstructure, success, errors = fetch_core(associated_structure, current_app.config['AWS_CONFIG'])
+                    if success and associated_structure != pdb_code:
+                        associated_structures[associated_structure] = thisstructure
     return {'structure':structure, 'pdb_code':pdb_code, 'features':fetch_constants('features'), 'chain_types':fetch_constants('chains'), 'associated_structures':associated_structures, 'display': display, 'message':message}
